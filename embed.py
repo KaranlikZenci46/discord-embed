8c9fa3be98951261110006642a269bf1628d741e
KaranlikZenci46 <153350370+KaranlikZenci46@users.noreply.github.com>
2024-07-23 17:52:16 +0300

    Add files via upload

---
+++ embed.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+from discord.ui import Button, View
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+TOKEN = 'your_discord_token'
+CHANNEL_ID = 1258067234236923955
+
+@bot.event
+async def on_ready():
+    print('SONUNDA AÇILDI AMK')
+
+    embed = discord.Embed(title="TITLE", description="Description", color=0x000000)
+
+    embed.set_image(url="img url")
+ 
+    button = Button(style=discord.ButtonStyle.link, label="Button", url="in button url")
+
+    view = View()
+    view.add_item(button)
+
+    channel = bot.get_channel(CHANNEL_ID)
+    if channel:
+        await channel.send(embed=embed, view=view)
+        print(f'Mesaj {channel.name} adlı kanala gönderildi')
+    else:
+        print("Channel is not exist")
+        
+
+bot.run(TOKEN)
+
+#made by https://github.com/KaranlikZenci46
+#discord.gg/vokkch
